435b2f56cf3ca5b3f08df4f180ca65f98657e65c
Akshay Kumar <akjaat.ak47@gmail.com>
2025-05-15 22:51:39 +0530

    Slug is removed

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,19 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+
 
 class Customer extends Model
 {
-    use HasFactory, Sluggable, SluggableScopeHelpers;
+    use HasFactory;
 
-    public function sluggable(): array
-    {
-        return [
-            'slug' => [
-                'source' => 'name'
-            ]
-        ];
-    }
+   
 }
